a32de1607410cfef2476ac3e5cd53143da6c644b
CarlosAl21 <g4to101@hotmail.com>
2026-05-02 20:24:52 -0500

    feat: agregar CalentamientoEjercicio al módulo de PlanEntrenamiento

--- src/plan-entrenamiento/plan-entrenamiento.module.ts
+++ src/plan-entrenamiento/plan-entrenamiento.module.ts
@@ -6,9 +6,10 @@ import { PlanEntrenamiento } from './entities/plan-entrenamiento.entity';
 import { Usuario } from 'src/usuario/entities/usuario.entity';
 import { RutinaDia } from 'src/rutina-dia/entities/rutina-dia.entity';
 import { RutinaEjercicio } from 'src/rutina-ejercicio/entities/rutina-ejercicio.entity';
+import { CalentamientoEjercicio } from 'src/rutina-ejercicio/entities/calentamiento-ejercicio.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PlanEntrenamiento, Usuario, RutinaDia, RutinaEjercicio])],
+  imports: [TypeOrmModule.forFeature([PlanEntrenamiento, Usuario, RutinaDia, RutinaEjercicio, CalentamientoEjercicio])],
   controllers: [PlanEntrenamientoController],
   providers: [PlanEntrenamientoService],
 })
