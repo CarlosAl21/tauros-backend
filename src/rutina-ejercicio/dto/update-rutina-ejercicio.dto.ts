1e04bfb1db833746e9ddcee310c11d2232e1b3ad
CarlosAl21 <g4to101@hotmail.com>
2026-05-02 19:02:36 -0500

    feat: eliminar validaciones de rutinaEjercicioId en el DTO de actualización de rutina de ejercicio

--- src/rutina-ejercicio/dto/update-rutina-ejercicio.dto.ts
+++ src/rutina-ejercicio/dto/update-rutina-ejercicio.dto.ts
@@ -1,10 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRutinaEjercicioDto } from './create-rutina-ejercicio.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateRutinaEjercicioDto extends PartialType(CreateRutinaEjercicioDto) {
-
-    @IsString()
-    @IsNotEmpty()
-    rutinaEjercicioId: string;
 }
